75794999760b6d9eadfdf12d64ae30bd4b5d7fcc
JustMrArgus <argusprogramming@protonmail.com>
2025-12-20 00:13:54 +0200

    Add endpoints skeleton and healthcheck

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { GamesModule } from './games/games.module';
+import { CartsModule } from './carts/carts.module';
+import { CartItemsModule } from './cart-items/cart-items.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, GamesModule, CartsModule, CartItemsModule],
   controllers: [AppController],
   providers: [AppService],
 })
