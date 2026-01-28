0f2dec030b3ff9973f6010c6896a31071431ba2d
JustMrArgus <argusprogramming@protonmail.com>
2026-01-28 21:20:44 +0200

    Add error page

--- frontend/app/error.tsx
+++ frontend/app/error.tsx
@@ -1,7 +1,23 @@
-"use client";
+'use client';
 
-const Error = ({ error, reset }: { error: Error; reset: () => void }) => {
-  return <div>Error</div>;
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
+
+const Error = () => {
+  return (
+    <div className="w-full h-[90vh] flex justify-center items-center">
+      <Alert
+        variant="destructive"
+        className="max-w-md bg-[#353538] border-none p-10"
+      >
+        <AlertTitle className="text-2xl font-bold">
+          The error occured!
+        </AlertTitle>
+        <AlertDescription className="font-bold">
+          Terrible error happend. Try again later
+        </AlertDescription>
+      </Alert>
+    </div>
+  );
 };
 
 export default Error;
--- frontend/app/loading.tsx
+++ frontend/app/loading.tsx
@@ -1,10 +1,10 @@
-"use client";
+'use client';
 
-import { ClipLoader } from "react-spinners";
+import { ClipLoader } from 'react-spinners';
 
 const GamesLoading = () => {
   return (
-    <div className="flex w-full h-screen justify-center items-center">
+    <div className="flex w-full h-[90vh] justify-center items-center">
       <ClipLoader color="#FFFFFF" size={80} />
     </div>
   );
