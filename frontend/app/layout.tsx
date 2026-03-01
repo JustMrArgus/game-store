00aebd3645704f65d08dee3f1cb2c36c790c7e8a
JustMrArgus <argusprogramming@protonmail.com>
2026-03-02 00:28:12 +0200

    Add new provider to the main layout

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -5,6 +5,7 @@ import './globals.css';
 import { QueryProvider } from '@/components/providers/query-provider';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
+import { AuthProvider } from '@/components/providers/auth-provider';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -32,11 +33,13 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#101014] text-white`}
       >
         <QueryProvider>
+          <AuthProvider>
           <div className="min-h-screen flex flex-col">
             <Header />
             <main className="flex-1 px-55">{children}</main>
             <Footer />
           </div>
+          </AuthProvider>
         </QueryProvider>
       </body>
     </html>
