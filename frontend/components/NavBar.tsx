9d899e861caa41e21d213ae49a6995586cf7c87d
JustMrArgus <argusprogramming@protonmail.com>
2026-03-02 00:28:34 +0200

    Modify navbar logic

--- frontend/components/NavBar.tsx
+++ frontend/components/NavBar.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import {
   NavigationMenu,
   NavigationMenuItem,
@@ -7,34 +9,53 @@ import {
 import { navigationMenuTriggerStyle } from '@/components/ui/navigation-menu';
 import { cn } from '@/lib/utils';
 import Link from 'next/link';
+import { useAuthStore } from '@/store/use-auth-store';
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
 
 const NavBar = () => {
+  const { isAuthenticated, user } = useAuthStore();
+
   return (
     <NavigationMenu>
-      <NavigationMenuList className="flex gap-5">
-        <NavigationMenuItem>
-          <NavigationMenuLink
-            asChild
-            className={cn(
-              navigationMenuTriggerStyle(),
-              'bg-[#353538] text-white hover:bg-[#ffffff59] hover:text-white transition duration-100',
-            )}
+      {!isAuthenticated ? (
+        <NavigationMenuList className="flex gap-5">
+          <NavigationMenuItem>
+            <NavigationMenuLink
+              asChild
+              className={cn(
+                navigationMenuTriggerStyle(),
+                'bg-[#353538] text-white hover:bg-[#ffffff59] hover:text-white transition duration-100',
+              )}
+            >
+              <Link href="/auth/login">Log in</Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuLink
+              asChild
+              className={cn(
+                navigationMenuTriggerStyle(),
+                'bg-[#353538] text-white hover:bg-[#ffffff59] hover:text-white transition duration-100',
+              )}
+            >
+              <Link href="/auth/signup">Sign Up</Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      ) : (
+        <NavigationMenuLink>
+          <Link
+            href={'/me'}
+            className="flex justify-center gap-2 text-[#cfcfd3] transition duration-300 hover:opacity-70 hover:cursor-pointer"
           >
-            <Link href="/auth/login">Log in</Link>
-          </NavigationMenuLink>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <NavigationMenuLink
-            asChild
-            className={cn(
-              navigationMenuTriggerStyle(),
-              'bg-[#353538] text-white hover:bg-[#ffffff59] hover:text-white transition duration-100',
-            )}
-          >
-            <Link href="/auth/signup">Sign Up</Link>
-          </NavigationMenuLink>
-        </NavigationMenuItem>
-      </NavigationMenuList>
+            <Avatar>
+              <AvatarImage src="https://github.com/shadcn.png" />
+              <AvatarFallback>PFP</AvatarFallback>
+            </Avatar>
+            <p>{user?.name}</p>
+          </Link>
+        </NavigationMenuLink>
+      )}
     </NavigationMenu>
   );
 };
